b7d381ec43160ce3897d5f255c54b112c0400f39
Raphaël Thériault <raphael_theriault@outlook.com>
2020-02-02 15:53:08 -0500

    Basic main

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ extern crate diesel;
 mod models;
 mod schema;
 
-fn main() {
-    println!("Hello, world!");
+use warp::{http::Response, Filter};
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    let not_found = warp::any().map(|| Response::builder().status(404).body("Not Found").unwrap());
+
+    let routes = hello.or(not_found);
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
